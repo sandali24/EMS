a723b55b4ce68ff493d0ac7f6ef52578395137f3
sandali24 <sandalisiri58@gmail.com>
2024-03-22 10:44:31 +0530

    jwt generate

---
+++ EmployeeMS/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Components/Login'
+
+function App() {
+  
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
